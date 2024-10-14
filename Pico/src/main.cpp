ebf53dada758715ef153cc136fc59a7246d8ad1e
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2024-10-14 13:14:29 -0500

    Add some error handling when BMP3888 is not detected, set up filters for BMP388 (TODO: Tune later)

--- Pico/src/main.cpp
+++ Pico/src/main.cpp
@@ -20,12 +20,15 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BMP3XX bmp;
-// put function declarations here:
+
 
 
 void setup() {
   // put your setup code here, to run once:
     Serial.begin(9600); //Begin broadcasting over serial on a baud rate of 9600
+    while(!Serial); //Execute while not running in serial mode
+
+
   // Set up pins for digital input and output- for help refer to the "Resources" folder for the schematic
   // Using pins 1-6 for servo output. Pin 3 is GROUND- Do not assign something to this
 
@@ -42,6 +45,20 @@ void setup() {
   pinMode(26, INPUT); //SDA-BNO055- SDA = serial input to processor- confirm
 
   pinMode(40, OUTPUT); //Output to Libre
+
+  //Check that we are receiving input from the BMP 388, print error message if not
+
+  if(! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)){
+    Serial.println("No BMP388 sensor is detected. Please check wiring, pin assignment in both hardware and software,etc. ");
+    while (1); // Throw
+  }
+
+  // TODO: Tune this - currently based off of the example code: https://learn.adafruit.com/adafruit-bmp388-bmp390-bmp3xx?view=all#spi-logic-pins-3022081
+  // TODO: Figure out what different sampling configs do
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 
 
 }
