18a78f566cbc085797a657418397e7e4d0456962
Wyatt Howard <wyattahoward@gmail.com>
2024-10-23 03:55:15 -0500

    new package

---
+++ ROS2/ros2_ws/src/data_relay/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'data_relay'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='wyatt',
+    maintainer_email='wyattahoward@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "talker_node = data_relay.talker:main",
+            "listener_node = data_relay.listener:main"
+        ],
+    },
+)
