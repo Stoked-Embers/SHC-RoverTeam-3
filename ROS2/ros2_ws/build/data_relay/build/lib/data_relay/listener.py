2215e7ef68d4caa4d3340f04729d1e16c939c550
Wyatt Howard <wyattahoward@gmail.com>
2024-10-24 19:28:50 -0500

    xbox controller partial integration

--- ROS2/ros2_ws/build/data_relay/build/lib/data_relay/listener.py
+++ ROS2/ros2_ws/build/data_relay/build/lib/data_relay/listener.py
@@ -9,7 +9,7 @@ class ListenerNode(Node):
     def __init__(self):
         super().__init__("listener_node")
         self.listener_ = self.create_subscription(
-            String, 'motor_topic', self.listener_callback, 10)
+            String, '/pico/command', self.listener_callback, 10)
         self.listener_
 
     def listener_callback(self, msg):
