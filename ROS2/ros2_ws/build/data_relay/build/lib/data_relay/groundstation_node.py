376cab8bd038b8e242dc82d2df2c57458f65979e
Wyatt Howard <wyattahoward@gmail.com>
2024-10-26 20:53:29 -0500

    implemented controls

--- ROS2/ros2_ws/build/data_relay/build/lib/data_relay/groundstation_node.py
+++ ROS2/ros2_ws/build/data_relay/build/lib/data_relay/groundstation_node.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 import pygame
+import time
 import threading
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -35,14 +36,16 @@ class TalkerNode(Node):
 
         self.motor_speed = 0
         self.selected_motor_speed = 0
-        self.joint0_angle = 90
-        self.joint1_angle = 90
-        self.joint2_angle = 90
+        self.joint0_angle = 0
+        self.joint1_angle = 0
+        self.joint2_angle = 0
+        self.joint3_angle = 0
         self.currentServo = 0
         self.servoSpeedArray = [1,5,10,15]
         self.servoIndex = 0
         self.servoSpeed = self.servoSpeedArray[self.servoIndex]
-        self.create_timer(0.1, self.timer_callback)
+        #self.create_timer(0.1, self.timer_callback)
+        self.timer_callback()
     
 #----------------------------------------------------------------------------------------------
 
@@ -80,99 +83,120 @@ class TalkerNode(Node):
 #----------------------------------------------------------------------------------------------
 
     def timer_callback(self):
-        events = pygame.event.get()
+        self.timer = pygame.time.Clock()
+        self.start_ui()
+        while self.running:
+            time.sleep(0.1)
+            events = pygame.event.get()
 
-        # BASE MOTOR CONTROLS
-        self.x_axis = self.joystick.get_axis(0)
-        if self.x_axis < -0.5:
-            self.motor_speed = -(self.selected_motor_speed)
-        elif self.x_axis > 0.5:
-            self.motor_speed = (self.selected_motor_speed)
-        else:
-            self.motor_speed = 0
-                
-        # SERVO MOVEMENT CONTROLS
-        self.y_axis = self.joystick.get_axis(4)
-        if self.y_axis < -0.5 and (self.currentServo == 0) and (self.joint0_angle >= 0): 
-            self.joint0_angle = min(130, self.joint0_angle + self.servoSpeed)
-        elif self.y_axis > 0.5 and (self.currentServo == 0) and (self.joint0_angle >= self.servoSpeed):
-            self.joint0_angle = min(130, self.joint0_angle - self.servoSpeed)
-        elif self.y_axis < -0.5 and (self.currentServo == 1) and (self.joint0_angle >= 0): 
-            self.joint0_angle = min(130, self.joint0_angle + self.servoSpeed)
-        elif self.y_axis > 0.5 and (self.currentServo == 1) and (self.joint0_angle >= self.servoSpeed):
-            self.joint0_angle = min(130, self.joint0_angle - self.servoSpeed)
-
-        # hat_x, hat_y = self.joystick.get_hat(0)
-
-        # SERVO SELECTION CONTROLS
-        # if abs(hat_x) > 0.5:
-        #     if self.joystick.get_hat(0) == (1,0) and self.currentServo == 0:
-        #         self.currentServo += 1
-        #         print(self.currentServo)
-        #     elif self.joystick.get_hat(0) == (1,0) and self.currentServo == 1:
-        #         self.currentServo -= 1
-        #         print(self.currentServo)
-        #     elif self.joystick.get_hat(0) == (-1,0) and self.currentServo == 0:
-        #         self.currentServo += 1
-        #         print(self.currentServo)
-        #     elif self.joystick.get_hat(0) == (-1,0) and self.currentServo == 1:
-        #         self.currentServo -= 1
-        #         print(self.currentServo)
-
-        # CHANGE MOTOR SPEED WITH KEYS ---- OUTDATED
-        for event in events:
-        #     if event.type == pygame.QUIT:
-        #         pygame.display.quit()
-        #         self.running = False
-        #     if event.type == pygame.KEYDOWN:
-        #         if event.key == pygame.K_LEFT and self.selected_motor_speed > 0:
-        #             self.selected_motor_speed -= 10
-        #             self.update_global_speed()
-        #         if event.key == pygame.K_RIGHT and self.selected_motor_speed< 100:
-        #             self.selected_motor_speed += 10
-        #             self.update_global_speed()
-
-            #if event.type == pygame.JOYAXISMOTION:
-                
-
-            if event.type == pygame.JOYBUTTONDOWN:
-                if self.joystick.get_button(1) and self.selected_motor_speed < 100:
-                    self.selected_motor_speed += 10
-                    self.update_global_speed()
-                if self.joystick.get_button(3) and self.selected_motor_speed > 0:
-                    self.selected_motor_speed -= 10
-                    self.update_global_speed()
+            # BASE MOTOR CONTROLS
+            self.x_axis = self.joystick.get_axis(0)
+            if self.x_axis < -0.5:
+                self.motor_speed = -(self.selected_motor_speed)
+            elif self.x_axis > 0.5:
+                self.motor_speed = (self.selected_motor_speed)
+            else:
+                self.motor_speed = 0
+                    
+            # SERVO MOVEMENT CONTROLS
+            self.y_axis = self.joystick.get_axis(4)
+            # Servo 0
+            if (self.currentServo == 0):
+                if self.y_axis < -0.5 and (self.joint0_angle >= 0): 
+                    self.joint0_angle = min(130, self.joint0_angle + self.servoSpeed)
+                elif self.y_axis > 0.5:
+                    if((self.joint0_angle - self.servoSpeed) < 0):
+                        self.joint0_angle = 0
+                    else:
+                        self.joint0_angle = min(130, self.joint0_angle - self.servoSpeed)
+            # Servo 1
+            elif (self.currentServo == 1):
+                if self.y_axis < -0.5 and (self.joint1_angle >= 0): 
+                    self.joint1_angle = min(130, self.joint1_angle + self.servoSpeed)
             
-            hat_x, hat_y = self.joystick.get_hat(0)
-            if abs(hat_x) > 0.5:
-                if self.joystick.get_hat(0) == (1,0) and self.currentServo == 0:
-                    self.currentServo += 1
-                    print(self.currentServo)
-                elif self.joystick.get_hat(0) == (1,0) and self.currentServo == 1:
-                    self.currentServo -= 1
-                    print(self.currentServo)
-                elif self.joystick.get_hat(0) == (-1,0) and self.currentServo == 0:
-                    self.currentServo += 1
-                    print(self.currentServo)
-                elif self.joystick.get_hat(0) == (-1,0) and self.currentServo == 1:
-                    self.currentServo -= 1
-                    print(self.currentServo)
-            if abs(hat_y) > 0.5:
-                if self.joystick.get_hat(0) == (0,1) and self.servoIndex < 3:
-                    self.servoIndex += 1
-                    self.servoSpeed = self.servoSpeedArray[self.servoIndex]
-                    print(self.servoSpeed)
-                elif self.joystick.get_hat(0) == (0,-1) and self.servoIndex > 0:
-                    self.servoIndex -= 1
-                    self.servoSpeed = self.servoSpeedArray[self.servoIndex]
-                    print(self.servoSpeed)
+                elif self.y_axis > 0.5:
+                    if((self.joint1_angle - self.servoSpeed) < 0):
+                        self.joint1_angle = 0
+                    else:
+                        self.joint1_angle = min(130, self.joint1_angle - self.servoSpeed)
+            # Servo 2
+            elif (self.currentServo == 2):
+                if self.y_axis < -0.5 and (self.joint2_angle >= 0): 
+                    self.joint2_angle = min(130, self.joint2_angle + self.servoSpeed)
+            
+                elif self.y_axis > 0.5:
+                    if((self.joint2_angle - self.servoSpeed) < 0):
+                        self.joint2_angle = 0
+                    else:
+                        self.joint2_angle = min(130, self.joint2_angle - self.servoSpeed)
 
-        
+            # END EFFECTOR
+            if self.joystick.get_button(6) and not self.joystick.get_button(7):
+                if((self.joint3_angle - 3) < 0):
+                    self.joint3_angle = 0
+                else:
+                    self.joint3_angle = min(130, self.joint3_angle - 3)
+            elif self.joystick.get_button(7) and not self.joystick.get_button(6):
+                self.joint3_angle = min(130, self.joint3_angle + 3)
 
-        msg = String()
-        msg.data = str(f"base:{self.motor_speed},ser0:{self.joint0_angle},ser1:{self.joint1_angle},ser2:{self.joint2_angle}")
-        self.publisher_.publish(msg)
-        self.get_logger().info(msg.data)
+
+            # CHANGE MOTOR SPEED WITH KEYS ---- OUTDATED
+            for event in events:
+                if event.type == pygame.QUIT:
+                    pygame.display.quit()
+                    self.running = False
+            #     if event.type == pygame.KEYDOWN:
+            #         if event.key == pygame.K_LEFT and self.selected_motor_speed > 0:
+            #             self.selected_motor_speed -= 10
+            #             self.update_global_speed()
+            #         if event.key == pygame.K_RIGHT and self.selected_motor_speed< 100:
+            #             self.selected_motor_speed += 10
+            #             self.update_global_speed()
+
+                #if event.type == pygame.JOYAXISMOTION:
+                    
+
+                if event.type == pygame.JOYBUTTONDOWN:
+                    # BASE MOTOR SPEED
+                    if self.joystick.get_button(9) and self.selected_motor_speed < 100:
+                        self.selected_motor_speed += 10
+                        self.update_global_speed()
+                    if self.joystick.get_button(8) and self.selected_motor_speed > 0:
+                        self.selected_motor_speed -= 10
+                        self.update_global_speed()
+                    # CHANGE SERVO WITH SQUARE
+                    if self.joystick.get_button(1):
+                        if self.currentServo == 0:
+                            self.currentServo = 1
+                        elif self.currentServo == 1:
+                            self.currentServo = 2
+                        elif self.currentServo == 2:
+                            self.currentServo = 0
+                    elif self.joystick.get_button(3):
+                        if self.currentServo == 2:
+                            self.currentServo = 1
+                        elif self.currentServo == 1:
+                            self.currentServo = 0
+                        elif self.currentServo == 0:
+                            self.currentServo = 2
+                    # CHANGE SPEED WITH BUMPERS
+                    if self.joystick.get_button(4):
+                        if self.servoIndex > 0:
+                            self.servoIndex -= 1
+                            self.servoSpeed = self.servoSpeedArray[self.servoIndex]
+                            print(self.servoSpeed)
+                    if self.joystick.get_button(5):
+                        if self.servoIndex < 3:
+                            self.servoIndex += 1
+                            self.servoSpeed = self.servoSpeedArray[self.servoIndex]
+                            print(self.servoSpeed)
+                    
+
+
+            msg = String()
+            msg.data = str(f"base:{self.motor_speed},ser0:{self.joint0_angle},ser1:{self.joint1_angle},ser2:{self.joint2_angle},ser3:{self.joint3_angle}")
+            self.publisher_.publish(msg)
+            self.get_logger().info(msg.data)
 
         pygame.event.clear()
 
